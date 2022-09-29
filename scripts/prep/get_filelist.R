a79714f2c7fe1c062077e5e8f9b84a983024db77
LukasSchefczyk <Lukas.Schefczyk@lfu.rlp.de>
2022-09-29 11:55:04 +0200

    Added get_files_from_url function this function gets loop over  path list with map_dfr to get a dataframe/tibble back

--- scripts/prep/get_filelist.R
+++ scripts/prep/get_filelist.R
@@ -5,6 +5,7 @@
 library(tidyverse)
 library(rvest)
 library(glue)
+library(lubridate)
 
 basepath <- "https://opendata.dwd.de/climate_environment/CDC/observations_germany/phenology/"
 basepathftp <- "ftp://opendata.dwd.de/climate_environment/CDC/observations_germany/phenology/"
@@ -13,15 +14,50 @@ reporter <- "annual_reporters"
 
  
  
- url="https://opendata.dwd.de/weather/tree.html"
- html <- read_html(url)
- html %>%  
+url="https://opendata.dwd.de/weather/tree.html"
+html <- read_html(url)
+pathlist <- html %>%  
    html_elements("a") %>%  
    html_attr("href") %>% 
    str_subset("phenology") %>%  
-   str_subset("observations")
- 
- #Todo
- #map over urls get filelists, merge together to tbl
+   str_subset("observations") %>%  as_tibble_col(column_name="paths")
 
- 
+pathlist2 <- pathlist[3:4,]
+#Todo
+#map over urls get filelists, merge together to tbl
+
+
+get_file_names_from_url2 <- function(x) {
+   file <- read_html(x) %>%
+      html_elements("a") %>%
+      html_attr("href") %>%
+      # str_subset(".txt")  %>%
+      str_subset("../", negate = TRUE) %>%
+      as_tibble_col(column_name = "file")
+   
+   
+   meta <- read_html(x) %>%
+      html_elements("pre") %>%
+      html_text2()  %>%
+      str_split("\n") %>%
+      unlist() %>%
+      #str_subset(">") %>%
+      str_subset("../", negate = TRUE) %>%
+      str_match("\\s\\s*(.*?)\\s*\r") %>%  # match between string1(space \\s) and string2 ("\r")
+      .[, 2] %>%
+      as_tibble_col(column_name = "meta") %>%
+      drop_na() %>%
+      separate(meta, c("lm_datum", "size"), sep = "\\s\\s+") %>%
+      mutate(
+         lm_datum = dmy_hm(lm_datum),
+         size = as.integer(size),
+         path = x
+      )
+   
+   bind_cols(file, meta) %>%
+      relocate(path, .before = file)
+}
+
+
+filelistfull <- map_dfr(pathlist$paths, get_file_names_from_url2)
+  
