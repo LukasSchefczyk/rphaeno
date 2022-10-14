a37ea9e2ef0d05b0b2e38b8287a478ad10097557
LukasSchefczyk <Lukas.Schefczyk@lfu.rlp.de>
2022-10-14 09:02:33 +0200

    fixed some attributes in naturraum geopackage

--- scripts/prep/create_geospatial_data.R
+++ scripts/prep/create_geospatial_data.R
@@ -1,3 +1,9 @@
 # Create sf objects from Lat lon data,
 #import of Borders of state and nature  from gpkg 
 #Namen cleanen von Naturraum etc , Alte Pheno skripts anschauen
+
+library(sf)
+gfile <- system.file("data/Naturraum_Grenzen_DE.gpkg", package = "sf", mustWork = TRUE)
+gfile <- "data/Naturraum_Grenzen_DE.gpkg"
+sf::st_layers(gfile)
+read_sf("data/Naturraum_Grenzen_DE.gpkg")
