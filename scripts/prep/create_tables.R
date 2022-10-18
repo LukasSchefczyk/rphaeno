f60ea96ef8399148133b8bd41b360a37b927f5fd
LukasSchefczyk <Lukas.Schefczyk@lfu.rlp.de>
2022-10-18 15:19:40 +0200

    added phase_englisch to megaframe

--- scripts/prep/create_tables.R
+++ scripts/prep/create_tables.R
@@ -29,15 +29,204 @@ create_megaframe <- function () {
   Pflanze <- tbl(con, "Pflanze") 
   Daten <- tbl(con,"Daten")
   Phasendefinition <- tbl(con,"Phasendefinition")
+  Phase <- tbl(con,"Phase")
   Obst_spezi <- tbl(con, "Obst_Spezifizierung")
   Mais_spezi <- tbl(con, "Mais_Spezifizierung")
   megaframe <- left_join(Daten,Stationen,by="stations_id") %>%
-  left_join(Phasendefinition,by=c("objekt_id","phasen_id")) %>% 
+  left_join(Phasendefinition,by=c("objekt_id","phasen_id")) %>%
+  left_join(Phase,by=c("phase","phasen_id")) %>%
   left_join(Pflanze,by=c("objekt_id","objekt")) 
   
   return(megaframe)
 }
 megaframe <- create_megaframe()
+
+megaframe %>% show_query() %>%  
+capture.output() %>% 
+  `[<-`(1, "CREATE VIEW megaframe AS") %>%
+  paste(collapse = " \n ") -> viewquery
+
+
+DBI::dbExecute(con, viewquery)
+
+#View erstellen 
+#nrow <- dbExecute(con, paste0("CREATE VIEW test AS",
+#                              "Random statements"))
+
+dbExecute(con, paste0("CREATE VIEW test AS SELECT
+`stations_id`,
+`referenzjahr`,
+`qualitaetsniveau`,
+`LHS`.`objekt_id` AS `objekt_id`,
+`phasen_id`,
+`eintrittsdatum`,
+`eintrittsdatum_qb`,
+`jultag`,
+`stationsname`,
+`geograph.breite`,
+`geograph.laenge`,
+`stationshoehe`,
+`naturraumgruppe_code`,
+`naturraumgruppe`,
+`naturraum_code`,
+`naturraum`,
+`datum_stationsaufloesung`,
+`bundesland`,
+`LHS`.`objekt` AS `objekt`,
+`phase`,
+`phasendefinition`,
+`bbch_code`,
+`hinweis_bbch`,
+`objekt_englisch`,
+`objekt_latein`
+FROM (
+  SELECT
+  `stations_id`,
+  `referenzjahr`,
+  `qualitaetsniveau`,
+  `LHS`.`objekt_id` AS `objekt_id`,
+  `LHS`.`phasen_id` AS `phasen_id`,
+  `eintrittsdatum`,
+  `eintrittsdatum_qb`,
+  `jultag`,
+  `stationsname`,
+  `geograph.breite`,
+  `geograph.laenge`,
+  `stationshoehe`,
+  `naturraumgruppe_code`,
+  `naturraumgruppe`,
+  `naturraum_code`,
+  `naturraum`,
+  `datum_stationsaufloesung`,
+  `bundesland`,
+  `objekt`,
+  `phase`,
+  `phasendefinition`,
+  `bbch_code`,
+  `hinweis_bbch`
+  FROM (
+    SELECT
+    `LHS`.`stations_id` AS `stations_id`,
+    `referenzjahr`,
+    `qualitaetsniveau`,
+    `objekt_id`,
+    `phasen_id`,
+    `eintrittsdatum`,
+    `eintrittsdatum_qb`,
+    `jultag`,
+    `stationsname`,
+    `geograph.breite`,
+    `geograph.laenge`,
+    `stationshoehe`,
+    `naturraumgruppe_code`,
+    `naturraumgruppe`,
+    `naturraum_code`,
+    `naturraum`,
+    `datum_stationsaufloesung`,
+    `bundesland`
+    FROM `Daten` AS `LHS`
+    LEFT JOIN `Stationen` AS `RHS`
+    ON (`LHS`.`stations_id` = `RHS`.`stations_id`)
+  ) AS `LHS`
+  LEFT JOIN `Phasendefinition` AS `RHS`
+  ON (
+    `LHS`.`objekt_id` = `RHS`.`objekt_id` AND
+    `LHS`.`phasen_id` = `RHS`.`phasen_id`
+  )
+) AS `LHS`
+LEFT JOIN `Pflanze` AS `RHS`
+ON (`LHS`.`objekt_id` = `RHS`.`objekt_id` AND `LHS`.`objekt` = `RHS`.`objekt`)" )) 
+
+
+
+
+
+
+dbExecute(con, paste0("CREATE VIEW test AS SELECT
+`stations_id`,
+`referenzjahr`,
+`qualitaetsniveau`,
+`LHS`.`objekt_id` AS `objekt_id`,
+`phasen_id`,
+`eintrittsdatum`,
+`eintrittsdatum_qb`,
+`jultag`,
+`stationsname`,
+`geograph.breite`,
+`geograph.laenge`,
+`stationshoehe`,
+`naturraumgruppe_code`,
+`naturraumgruppe`,
+`naturraum_code`,
+`naturraum`,
+`datum_stationsaufloesung`,
+`bundesland`,
+`LHS`.`objekt` AS `objekt`,
+`phase`,
+`phasendefinition`,
+`bbch_code`,
+`hinweis_bbch`,
+`objekt_englisch`,
+`objekt_latein`
+FROM (
+  SELECT
+  `stations_id`,
+  `referenzjahr`,
+  `qualitaetsniveau`,
+  `LHS`.`objekt_id` AS `objekt_id`,
+  `LHS`.`phasen_id` AS `phasen_id`,
+  `eintrittsdatum`,
+  `eintrittsdatum_qb`,
+  `jultag`,
+  `stationsname`,
+  `geograph.breite`,
+  `geograph.laenge`,
+  `stationshoehe`,
+  `naturraumgruppe_code`,
+  `naturraumgruppe`,
+  `naturraum_code`,
+  `naturraum`,
+  `datum_stationsaufloesung`,
+  `bundesland`,
+  `objekt`,
+  `phase`,
+  `phasendefinition`,
+  `bbch_code`,
+  `hinweis_bbch`
+  FROM (
+    SELECT
+    `LHS`.`stations_id` AS `stations_id`,
+    `referenzjahr`,
+    `qualitaetsniveau`,
+    `objekt_id`,
+    `phasen_id`,
+    `eintrittsdatum`,
+    `eintrittsdatum_qb`,
+    `jultag`,
+    `stationsname`,
+    `geograph.breite`,
+    `geograph.laenge`,
+    `stationshoehe`,
+    `naturraumgruppe_code`,
+    `naturraumgruppe`,
+    `naturraum_code`,
+    `naturraum`,
+    `datum_stationsaufloesung`,
+    `bundesland`
+    FROM `Daten` AS `LHS`
+    LEFT JOIN `Stationen` AS `RHS`
+    ON (`LHS`.`stations_id` = `RHS`.`stations_id`)
+  ) AS `LHS`
+  LEFT JOIN `Phasendefinition` AS `RHS`
+  ON (
+    `LHS`.`objekt_id` = `RHS`.`objekt_id` AND
+    `LHS`.`phasen_id` = `RHS`.`phasen_id`
+  )
+) AS `LHS`
+LEFT JOIN `Pflanze` AS `RHS`
+ON (`LHS`.`objekt_id` = `RHS`.`objekt_id` AND `LHS`.`objekt` = `RHS`.`objekt`)" )) 
+
+
 
 Daten_rlp <- megaframe %>% filter(bundesland %in% c("Rheinland-Pfalz")) %>% collect()
 
