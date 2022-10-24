16fa03a03b1aa359e6c10e1873c87fccb122e9fe
LukasSchefczyk <Lukas.Schefczyk@lfu.rlp.de>
2022-10-24 15:22:45 +0200

    modified

--- scripts/prep/create_tables.R
+++ scripts/prep/create_tables.R
@@ -17,65 +17,6 @@ Daten <- tbl(con2,"Daten")
 Phasendefinition <- tbl(con2,"Phasendefinition")
 Phase <- Phasendefinition %>%  select(phase_id,phase) %>% arrange(phase_id) %>% distinct() %>%  collect() 
 
-station_s <- tbl(con2,"Phaenologie_Stationen_Sofortmelder")
-station_j <- tbl(con2,"Phaenologie_Stationen_Jahresmelder")
-station_s_only <-anti_join(station_s,station_j,by="stations_id") %>% mutate(Melder="Sofortmelder") %>% collect()
-station_j_only <-anti_join(station_j,station_s,by="stations_id") %>% mutate(Melder="Jahresmelder") %>% collect()
-station_sj <- semi_join(station_j,station_s,by="stations_id") %>% mutate(Melder="Beides") %>% collect()
-lala <- bind_rows(station_s_only,station_sj,station_j_only)
-
-create_megaframe <- function (con=con) {
-  
-  #con <- DBI::dbConnect(RSQLite::SQLite(), dbname = dbname )
-  con <- con
-  Stationen <- tbl(con, "Stationen") 
-  Pflanze <- tbl(con, "Pflanze") 
-  Daten <- tbl(con,"Daten")
-  Phasendefinition <- tbl(con,"Phasendefinition")
-  Phase <- tbl(con,"Phase")
-  Obst_spezi <- tbl(con, "Obst_Spezifizierung")
-  Mais_spezi <- tbl(con, "Mais_Spezifizierung")
-  megaframe <- left_join(Daten,Stationen,by="stations_id") %>%
-  left_join(Phasendefinition,by=c("pflanze_id","phase_id")) %>%
-  left_join(Phase,by=c("phase","phase_id")) %>%
-  left_join(Pflanze,by=c("pflanze_id","pflanze")) 
-  
-  #DBI::dbDisconnect(con)
-  
-  return(megaframe)
-}
-
-create_view_in_db <- function(df,viewname,con=con) {
-  #con <- DBI::dbConnect(RSQLite::SQLite(), dbname = dbname )
-  con <- con 
-  require(dbplyr)
-  #df <- tibble(...)
-  df %>% 
-    show_query() %>%  
-    capture.output() %>% 
-    #erste Zeile vom SQL befehl austauschen gegen create view dbplyr kann keine views erstellen
-    `[<-`(1, paste0("CREATE VIEW ",viewname," AS")) %>%
-    paste(collapse = " \n ") -> viewquery
-  DBI::dbExecute(con, viewquery)
-  #DBI::dbDisconnect(con)
-}
-
-megaframe <- create_megaframe(con2) %>% create_view_in_db("megatest",con2)
-
-
-
-megaframe %>% show_query() %>%  
-capture.output() %>% 
-  #erste Zeile vom SQL befehl austauschen gegen create view dbplyr kann keine views erstellen
-  `[<-`(1, "CREATE VIEW megaframe AS") %>%
-  paste(collapse = " \n ") -> viewquery
-
-
-DBI::dbExecute(con, viewquery)
-
-megaframe <- tbl(con2,"Megaframe")
-
-
 Daten_rlp <- megaframe %>% filter(bundesland %in% c("Rheinland-Pfalz")) %>% collect()
 
 copy_to(con, daten_rlp, "daten_rlp",
@@ -96,5 +37,13 @@ copy_to(con,megaframe, "megaframe",
                        "naturraum_code","naturraum","objekt_id","objekt" ))
 }
 
-
 #DBI::dbDisconnect(con)  
+
+megaframe <- tbl(con,"Megaframe")
+Daten_rlp <- megaframe %>% 
+  filter(bundesland %in% c("Rheinland-Pfalz")) %>% 
+  collect()
+
+Kettenheim <- megaframe %>% filter(stationsname=="Kettenheim") %>% collect()
+Kettenheim %>% filter(phase_id==5,pflanze_id==210) %>%  ggplot() + aes(x=refjahr,y=jultag) + geom_line()
+
