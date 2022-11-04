6e42244b6155ff9d4e91e60d03539b23556e9e0d
LukasSchefczyk <Lukas.Schefczyk@lfu.rlp.de>
2022-11-04 11:33:01 +0100

    testing

--- scripts/prep/create_tables.R
+++ scripts/prep/create_tables.R
@@ -14,10 +14,12 @@ con <- con2
 Stationen <- tbl(con2, "Stationen") 
 Pflanze <- tbl(con2, "Pflanze") 
 Daten <- tbl(con2,"Daten")
+Daten_Jahresmelder <- tbl(con2,"Daten_Jahresmelder")
 Phasendefinition <- tbl(con2,"Phasendefinition")
 Phase <- Phasendefinition %>%  select(phase_id,phase) %>% arrange(phase_id) %>% distinct() %>%  collect() 
+megaframe_Jahresmelder <- tbl(con2,"Megaframe_Jahresmelder")
 
-Daten_rlp <- megaframe %>% filter(bundesland %in% c("Rheinland-Pfalz")) %>% collect()
+Daten_rlp <- megaframe_Jahresmelder %>% filter(bundesland %in% c("Rheinland-Pfalz")) %>% collect()
 
 copy_to(con, daten_rlp, "daten_rlp",
         temporary = FALSE,
@@ -48,3 +50,12 @@ Kettenheim <- megaframe %>% filter(stationsname=="Kettenheim") %>% collect()
 Kettenheim %>% filter(phase_id==5,pflanze_id==210) %>%  ggplot() + aes(x=refjahr,y=jultag) + geom_line()
 
 
+
+
+df <- Daten_rlp %>% filter(phase_id==5 & stationshoehe>200 & refjahr == 2021 & pflanze_id==215)
+
+df %>%  nr_df_to_sf(col2geom="naturraum_code",geomcol = "naturraum_code",clip=NULL) %>% 
+  ggplot() +
+  geom_sf(aes(fill=jultag,geometry=geom),show.legend = TRUE) +
+  geom_sf(data= df %>% 
+            station_df_to_sf() )
