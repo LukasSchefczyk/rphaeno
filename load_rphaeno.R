0815b9dfcae65884de301c3ff96acde138fa23d3
LukasSchefczyk <Lukas.Schefczyk@lfu.rlp.de>
2022-11-07 08:35:41 +0100

    create load script

---
+++ load_rphaeno.R
@@ -0,0 +1,15 @@
+#source this file to load phenoscripts into R
+#library dependencies
+require(tidyverse)
+require(rvest)
+require(glue)
+require(lubridate)
+require(RSQLite)
+require(DBI)
+require(dbplyr)
+require(sf)
+
+# main scripts
+source("scripts/prep/functions_db.R")
+source("scripts/prep/create_database.R")
+
