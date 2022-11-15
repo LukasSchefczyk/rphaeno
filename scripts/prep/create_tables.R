7d35097a0e87ce77bf161aecd884e9fe15feecc7
LukasSchefczyk <Lukas.Schefczyk@lfu.rlp.de>
2022-11-15 16:26:52 +0100

    Added alternative plants for pheno clock

--- scripts/prep/create_tables.R
+++ scripts/prep/create_tables.R
@@ -105,7 +105,12 @@ Notizen <- tbl(con,"Notizen")
 uhr_meta <- tibble(pflanze_id=c(113,109,310,129,130,311,129,132,132,132),
        pflanze=c("Hasel","Forsythie","Apfel","Schwarzer Holunder","Sommer-Linde","Apfel, frühe Reife","Schwarzer Holunder","Stiel-Eiche","Stiel-Eiche","Stiel-Eiche"),
        phase_id=c(5,5,5,5,5,29,62,62,31,32),
+       jahreszeit=c("Vorfrühling","Erstfrühling","Vollfrühling","Frühsommer","Hochsommer","Spätsommer","Frühherbst","Vollherbst","Spätherbst","Winter"),
+       alt_pflanze_id=c(127,350,132,121,360,103,119,122,103,313),
+       alt_pflanze=c("Schneeglöckchen","Stachelbeere","Stiel-Eiche","Robinie","Rote Johannisbeere","Eberesche","Kornelkirsche","Rosskastanie","Eberesche","Apfel, späte Reife"),
+       alt_phase_id=c(5,4,4,5,29,62,62,62,32,32),
        ) %>% mutate(order=1:length(pflanze))
+
 
 Daten_uhr <- tbl(con,"Megaframe_Jahresmelder") %>%
   filter(bundesland %in% "Rheinland-Pfalz" & pflanze_id %in% !!uhr_meta$pflanze_id & phase_id %in% !!uhr_meta$phase_id) %>% 
@@ -114,4 +119,10 @@ Daten_uhr <- tbl(con,"Megaframe_Jahresmelder") %>%
 Daten_uhr %>%  filter(pflanze_id==uhr_meta$pflanze_id[1] & phase_id==uhr_meta$phase_id[1]) %>% 
   summarise(mean_jultag=mean(jultag),order=uhr_meta$order[1])
 
-Daten_uhr %>% left_join(uhr_meta %>% select(-pflanze),by=c("pflanze_id","phase_id")) %>%  group_by(order) %>% drop_na(order) %>%  summarise(mean_jultag=mean(jultag)) 
+plot_data_uhr <- Daten_uhr %>% left_join(uhr_meta ,by=c("pflanze_id","phase_id"))  %>% 
+  filter(refjahr>=1961 & refjahr <=2021) %>%  
+  group_by(order) %>%
+  drop_na(order) %>% 
+  summarise(mean_jultag=mean(jultag),count=n()) %>%
+  right_join(uhr_meta,by="order") %>% 
+  print_all
